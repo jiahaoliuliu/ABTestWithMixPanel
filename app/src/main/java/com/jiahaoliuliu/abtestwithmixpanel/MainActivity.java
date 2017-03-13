8012224208e3c600ea436701f3645d579bcaf69d
Jiahao Liu <jiahaoliuliu@gmail.com>
2017-03-13 17:47:49 +0400

    Set basic A/B test on MixPanel

--- app/src/main/java/com/jiahaoliuliu/abtestwithmixpanel/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/abtestwithmixpanel/MainActivity.java
@@ -3,8 +3,10 @@ package com.jiahaoliuliu.abtestwithmixpanel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.mixpanel.android.mpmetrics.MixpanelAPI;
+import com.mixpanel.android.mpmetrics.Tweak;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "ABTestWithMixPanel";
     private MixpanelAPI mixpanel;
 
+    // A/B test values
+    private static Tweak<Boolean> showAds = MixpanelAPI.booleanTweak("Show ads", false);
+
+    // Views
+    private TextView mTitleTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
         mixpanel = MixpanelAPI.getInstance(this, APIToken.MIX_PANEL_TOKEN);
 
-        sendDummyEvent();
+        // Link the views
+        mTitleTextView = (TextView) findViewById(R.id.title);
+
+//        sendDummyEvent();
+
+        getABTest();
     }
 
     private void sendDummyEvent() {
@@ -32,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
             mixpanel.track("MainActivity - onCreate called", props);
         } catch (JSONException e) {
             Log.e(TAG, "Unable to add properties to JSONObject", e);
+        }
+    }
+
+    private void getABTest() {
+        if (showAds.get()) {
+            mTitleTextView.setText("Buy 100g of Chicke for 20AED!");
+        } else {
+            mTitleTextView.setText("Have a good day!");
         }
     }
 }
