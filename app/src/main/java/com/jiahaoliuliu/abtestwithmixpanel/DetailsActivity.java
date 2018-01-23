5583a2e0561c4296fb24f2ab075b8f398b946894
Jiahao Liu <jiahao.liu@careem.com>
2018-01-23 10:10:13 +0400

    Update the details activity with the data binding

--- app/src/main/java/com/jiahaoliuliu/abtestwithmixpanel/DetailsActivity.java
+++ app/src/main/java/com/jiahaoliuliu/abtestwithmixpanel/DetailsActivity.java
@@ -1,56 +1,57 @@
 package com.jiahaoliuliu.abtestwithmixpanel;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
-import com.mixpanel.android.mpmetrics.MixpanelAPI;
-import com.mixpanel.android.mpmetrics.Tweak;
+import com.jiahaoliuliu.abtestwithmixpanel.databinding.ActivityDetailsBinding;
 
 public class DetailsActivity extends AppCompatActivity {
 
     private static final String TAG = "DetailsActivity";
 
-    private TextView mTitleTextView;
-
     private AbTestManager mAbTestManager;
+    private ActivityDetailsBinding activityDetailsBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        activityDetailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_details);
 
-        // Link the views
-        mTitleTextView = (TextView) findViewById(R.id.title);
+        // Update the toolbar
+        setSupportActionBar(activityDetailsBinding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         mAbTestManager = new AbTestManager();
-
-        getABTest();
+        applyABTest();
     }
 
 
-    private void getABTest() {
+    private void applyABTest() {
         AbTestManager.AbTestResult abTestResult = mAbTestManager.getAbTestResult();
 
         Log.d(TAG, "The ab test result is " + abTestResult);
 
         switch (abTestResult) {
             case SHOW_ADS_DETAILS:
-                mTitleTextView.setText("Buy 100g of Chicke for 20AED!");
+                activityDetailsBinding.title.setText("Buy 100g of Chicke for 20AED!");
                 break;
             case SHOW_WARNING:
-                mTitleTextView.setText("Warning! You don't have more money!");
+                activityDetailsBinding.title.setText("Warning! You don't have more money!");
                 break;
             default:
             case DEFAULT:
-                mTitleTextView.setText("Have a good day!");
+                activityDetailsBinding.title.setText("Have a good day!");
                 break;
         }
     }
 
+    // Action bar
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
